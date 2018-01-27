82bb1a492334df9680993c717ae67174ed55a3f2
Maggot94 <andrescastropuerta@gmail.com>
2018-01-27 09:09:06 -0500

    GayJam Noche 1

---
+++ Assets/Scripts/CamerFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CamerFollow : MonoBehaviour {
+
+	public Transform target;
+	Camera mycam;
+
+	// Use this for initialization
+	void Start () {
+		mycam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target) {
+
+			transform.position = Vector3.Lerp (transform.position, target.position, 0.1f) + new Vector3 (0, 0, -10); 
+
+		}
+	}
+}
---
+++ Assets/Scripts/CamerSmooth.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamerSmooth : MonoBehaviour {
+	private Vector2 velocity;
+
+	public float smoothTimey;
+	public float smoothTimex;
+
+	public GameObject player;
+
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void LateUpdate()
+	{
+		float posx = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimex);
+		float posy = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimey);
+
+		transform.position = new Vector3 (posx, posy, transform.position.z);
+	}
+}
